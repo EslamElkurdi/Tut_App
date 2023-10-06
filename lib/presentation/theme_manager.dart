40f3fa3ae24c819bad0ed6dd767448bd3c2e9b9d
Eslam Gamal <elkurdieslam@gmail.com>
2023-10-06 20:06:17 +0300

    Adding AppBar Theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tut_app/presentation/color_manager.dart';
+import 'package:tut_app/presentation/fonts_manager.dart';
+import 'package:tut_app/presentation/styles_manager.dart';
 import 'package:tut_app/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme()
@@ -11,6 +13,7 @@ ThemeData getApplicationTheme()
     primaryColorDark: ColorManager.darkPrimary,
     primaryColorLight: ColorManager.lightGrey,
     disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.primaryOpacity70,
 
     // cardView Theme
     cardTheme: CardTheme(
@@ -20,7 +23,17 @@ ThemeData getApplicationTheme()
     ),
 
 
-    // AppBar Theme
+    // AppBar
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(
+          color: ColorManager.white,
+          fontSize: FontSize.s16
+      ),
+    ),
 
 
     // button Theme
