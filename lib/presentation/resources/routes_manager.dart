08a98a3cecb38664addb25cd54b6a68e1e30eba0
Eslam Gamal <elkurdieslam@gmail.com>
2023-10-07 14:49:39 +0300

    Adding Routes Generator Manager

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/forgot_password/forgot_password_view.dart';
+import 'package:tut_app/presentation/login/login_view.dart';
+import 'package:tut_app/presentation/main/main_view.dart';
+import 'package:tut_app/presentation/register/register_view.dart';
+import 'package:tut_app/presentation/splash/splash_view.dart';
+import 'package:tut_app/presentation/store_details/store_details_view.dart';
+
 class Routes
 {
   static const String splashRoute = "/";
@@ -6,4 +15,40 @@ class Routes
   static const String forgotPasswordRoute = "/forgotPassword";
   static const String mainRoute = "/main";
   static const String storeDetailsRoute = "/storeDetails";
+}
+
+class RoutesGenerator
+{
+  static Route<dynamic> getRoute(RouteSettings settings)
+  {
+    switch(settings.name)
+    {
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_)=> const ForgotPasswordView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_)=> const LoginView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_)=> const MainView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_)=> const RegisterView());
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_)=> const SplashView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_)=> const StoreDetailsView());
+      default:
+        return undefinedRoute();
+    }
+  }
+
+  static Route<dynamic> undefinedRoute()
+  {
+    return MaterialPageRoute(builder: (_)=> Scaffold(
+      appBar: AppBar(
+        title: const Text("No Route Found"), // move to string manager
+      ),
+      body: const Center(
+        child: Text("No Route Found"), // move to string manager
+      ),
+    ));
+  }
 }
